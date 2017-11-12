f8934c64f1fc1f3a5e0cfba6db2a50ad5c7604dd
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-11-11 19:12:17 -0600

    Updated smart contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,6 +2,13 @@ pragma solidity ^0.4.4;
 
 contract Adoption {
     address[16] public adopters;
+
+
+    function Adoption() public {
+        for (uint i=0; i < adopters.length; i++) {
+            adopters[i] = 0x0000000000000000000000000000000000000000;
+        }
+    }
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
